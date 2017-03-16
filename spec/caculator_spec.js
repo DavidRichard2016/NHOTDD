d523f5ce6d1febdb471437053e48b66e5026ee09
david <917930921@qq.com>
2017-03-16 22:54:03 +0800

    refactor caculator.js

--- spec/caculator_spec.js
+++ spec/caculator_spec.js
@@ -12,18 +12,47 @@ var caculator = require("../lib/caculator.js");
 describe("Caculating Word Componet Test", function(){
     sinon.spy(console, 'log');
 
-    it("should return 'hello 1' given 'hello' ", function(){
-      // given
-      var txt = "hello"
+    // it("should return 'hello 1' given 'hello' ", function(){
+    //   // given
+    //   var txt = "hello"
+    //
+    //   //when
+    //   var result = caculator(txt);
+    //
+    //   //then
+    //   var expectResult = {};
+    //   expectResult['hello']=1;
+    //   console.log(expectResult);
+    //   expect(result).to.deep.equal(expectResult);
+    // });
 
+    it("should return 'hello 4 test 3' given 'hello hello hello hello test test test' ", function(){
+      // given
+      var txt = ['hello', 'hello', 'hello', 'hello', 'test' ,'test' ,'test']
       //when
-      var result = caculator(txt);
+      var result = caculator.wordCount(txt);
 
       //then
-      var expectResult = {};
-      expectResult['hello']=1;
-      console.log(expectResult);
+      var expectResult = [
+        {name:'hello',count:4},
+        {name:'test',count:3},
+    ]
       expect(result).to.deep.equal(expectResult);
     });
+
+    // it("should return 'hello 4 test 3' given 'hello hello hello hello test test test' ", function(){
+    //   // given
+    //   var txt = {
+    //     {test}
+    //   }
+    //
+    //   //when
+    //   var result = caculator(txt);
+    //
+    //   //then
+    //   var expectResult = { name : "hello" ,count : }
+    //   expect(result).to.deep.equal(expectResult);
+    // });
+
 
 });
