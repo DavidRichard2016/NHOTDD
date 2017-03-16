a9f6e40bc8e06c28a254f154daa9da75ff8e1caf
david <917930921@qq.com>
2017-03-16 21:18:09 +0800

    init project 1. use npm manage code

---
+++ spec/caculator_spec.js
@@ -0,0 +1,29 @@
+"use strict";
+var _ = require("lodash");
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var expect = chai.expect;
+chai.use(sinonChai);
+
+var caculator = require("../lib/caculator.js");
+
+
+describe("Caculating Word Componet Test", function(){
+    sinon.spy(console, 'log');
+
+    it("should return 'hello 1' given 'hello' ", function(){
+      // given
+      var txt = "hello"
+
+      //when
+      var result = caculator(txt);
+
+      //then
+      var word = ['hello',1];
+      var expectResult = new Array(word);
+      console.log(expectResult);
+      expect(result).to.deep.equal(expectResult);
+    });
+
+});
